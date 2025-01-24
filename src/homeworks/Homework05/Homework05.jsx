69684152ca341f6acfb587b496216b720cceed6a
Ekaterina Zavertyaeva <katsiarynazavertyaeva@gmail.com>
2025-01-24 13:39:33 +0100

    add fix

--- src/homeworks/Homework05/Homework05.jsx
+++ src/homeworks/Homework05/Homework05.jsx
@@ -6,6 +6,7 @@ function Homework05() {
   const productsElements = products.map((product) => {
     return (
       <Product
+      // внутреннее свойство элементов, передаётся внутри метода map (через props в компонент передавать не нужно!)
         key={product.id}
         productName={product.name}
         productPrice={product.price}
